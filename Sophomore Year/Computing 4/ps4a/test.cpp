b7e62def8edf24b7233856ce58ff0acfe484a68e
cambenassi <cambenassi@gmail.com>
2021-03-22 16:10:55 -0400

    Update test.cpp

--- Sophomore Year/Computing 4/ps4a/test.cpp
+++ Sophomore Year/Computing 4/ps4a/test.cpp
@@ -3,6 +3,10 @@
 
 #include "CircularBuffer.h"
 
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE main
+#include <boost/test/unit_test.hpp>
+
 int main(int argc, char* argv[]){
 
     CircularBuffer test(1);
