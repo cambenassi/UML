157511ac97c68f5609e9ad67c9d4de05ee404952
cambenassi <cambenassi@gmail.com>
2021-04-01 16:49:49 -0400

    create ps5

---
+++ Sophomore Year/Computing 4/ps5/TFractal.cpp
@@ -0,0 +1,13 @@
+// Copyright 2021 Cameron Benassi @ UML. All rights reserved.
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    double length = (argv[1]);
+    int depth = (argv[2]);
+
+    std::cout << length << " " << depth << std::endl;
+
+
+    return 0;
+}
