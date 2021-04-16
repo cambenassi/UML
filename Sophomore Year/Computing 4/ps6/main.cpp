733d82b4b4c625b718169f3cf920ac1fe44d8167
Cam Benassi <cambenassi@pop-os.localdomain>
2021-04-16 17:16:04 -0400

    writing main

--- Sophomore Year/Computing 4/ps6/main.cpp
+++ Sophomore Year/Computing 4/ps6/main.cpp
@@ -2,6 +2,17 @@
 
 #include <iostream>
 
+#include <boost/regex.hpp>
+
 int main(int argc, char* argv[]) {
+    std::string log = argv[1];
+    log = log + ".rpt";
+    FILE *pFile = fopen(log.c_str(), "w");
+    
+    
+    
+    
+    
+    fclose(pFile);
     return 0;
 }
