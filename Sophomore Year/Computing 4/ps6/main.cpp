b2e0eb9c3d06547a671de2ba0c4860181b8a60f2
cambenassi <cambenassi@gmail.com>
2021-04-19 23:24:37 -0400

    regex update

--- Sophomore Year/Computing 4/ps6/main.cpp
+++ Sophomore Year/Computing 4/ps6/main.cpp
@@ -1,32 +1,37 @@
 // Copyright 2021 Cameron Benassi @ UML. All rights reserved.
 
-
 #include <iostream>
 #include <fstream>
-#include <regex>
 
-#include <boost/regex.hpp>
+#include <regex> //NOLINT
+
+// #include <boost/regex.hpp>
 
 int main(int argc, char* argv[]) {
-    std::string log = argv[1];
-    std::string outLog = log + ".rpt";
-    std::string temp;
+    std::string log = argv[1], outLog = log + ".rpt", temp;
     std::ifstream input(log);
+    std::smatch smatches;
+    std::regex startup("server started"),
+    success("oejs.AbstractConnector:Started SelectChannelConnector");
     FILE *output = fopen(outLog.c_str(), "w");
-    int numScanned = 0, initiated = 0, completed = 0;
-    
-    fprintf(output, "Device Boot Report\n\nInTouch log file: %s\n", 
-    log.c_str());
-    fprintf(output, "Lines scanned: ");
-    
+    int numScanned = 0;
+    bool start = false;
+
+    fprintf(output, "Device Boot Report\n\nInTouch log file: %s\n-------------------------------------\n", log.c_str()); //NOLINT
+
     while (std::getline(input, temp)) {
-        
-        
+        if (std::regex_search(temp, smatches, startup)) {
+            fprintf(output, "\nStartup - Line: %d\tDate & Time: NYI\nStatus - ", numScanned); //NOLINT
+            start = true;
+        }
+        if (start && std::regex_search(temp, smatches, success)) {
+            fprintf(output, "Success, finished on line %d", numScanned); //NOLINT
+            start = false;
+        }  // fix failure, add date & time
         numScanned++;
     }
-    fprintf(output, "%d", numScanned);
-    
-    
+    fprintf(output, "Lines scanned: %d", numScanned);
+
     input.close();
     fclose(output);
     return 0;
