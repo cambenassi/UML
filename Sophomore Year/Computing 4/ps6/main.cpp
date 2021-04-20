cf491855a5163a65bcee25ea3327309fd1d04475
Cam Benassi <cambenassi@pop-os.localdomain>
2021-04-19 22:41:09 -0400

    ps6 work

--- Sophomore Year/Computing 4/ps6/main.cpp
+++ Sophomore Year/Computing 4/ps6/main.cpp
@@ -1,18 +1,33 @@
 // Copyright 2021 Cameron Benassi @ UML. All rights reserved.
 
+
 #include <iostream>
+#include <fstream>
+#include <regex>
 
 #include <boost/regex.hpp>
 
 int main(int argc, char* argv[]) {
     std::string log = argv[1];
-    log = log + ".rpt";
-    FILE *pFile = fopen(log.c_str(), "w");
+    std::string outLog = log + ".rpt";
+    std::string temp;
+    std::ifstream input(log);
+    FILE *output = fopen(outLog.c_str(), "w");
+    int numScanned = 0, initiated = 0, completed = 0;
+    
+    fprintf(output, "Device Boot Report\n\nInTouch log file: %s\n", 
+    log.c_str());
+    fprintf(output, "Lines scanned: ");
+    
+    while (std::getline(input, temp)) {
+        
+        
+        numScanned++;
+    }
+    fprintf(output, "%d", numScanned);
     
     
-    
-    
-    
-    fclose(pFile);
+    input.close();
+    fclose(output);
     return 0;
 }
